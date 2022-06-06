c408155675726b1e6ad955d8fc28d89158f7441d
Chinni <enjarappachinni2001@gmail.com>
2022-06-07 00:06:26 +0530

    unit6

---
+++ day2/demo-app/src/component/List.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const List = () =>{
+    return(
+        <>
+        <h1>Mobile Opertaing System</h1>
+        <ul>
+            <li>Android</li>
+            <li>BlockBerry</li>
+            <li>iphone</li>
+            <li>Windows Phone</li>
+        </ul>
+        <h1>Mobile Manufacturing</h1>
+        <li>Sumsung</li>
+        <li>HTC</li>
+        <li>Micromax</li>
+        <li>Apple</li>
+
+        </>
+
+    )
+   
+
+
+}
+export default List;
