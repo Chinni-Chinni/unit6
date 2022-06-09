88266995235575bb48523fcde6cf49727529686f
Chinni <enjarappachinni2001@gmail.com>
2022-06-09 17:57:31 +0530

    cim

--- assignments/grocerylist/src/todo/todolist.jsx
+++ assignments/grocerylist/src/todo/todolist.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+
 const TodoList = ({ data }) => {
   //  console.log(data);
   return <>
     {data.map((item) =>
-    (<h1> {item.title}</h1>
+    (
+      <div id="inputlist">
+        <h3>{item.title}</h3>
+        < h2 > {item.status ? "Completed" : "Not Completed"}</h2>
+        <button>DELETE</button>
+
+      </div>
+
     ))}
 
   </>
